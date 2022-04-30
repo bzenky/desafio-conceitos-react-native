7344fc9c70456fee4549dedcdc6182ffafb19f77
Zenky <bzenky@gmail.com>
2022-04-30 18:36:02 -0300

    tasks list, todo input, home

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -10,7 +10,7 @@ export function TodoInput({ addTask }: TodoInputProps) {
   const [task, setTask] = useState('');
 
   function handleAddNewTask() {
-    if (task) {
+    if (task && task.trim() !== '') {
       addTask(task);
       setTask('');
     }
@@ -24,6 +24,7 @@ export function TodoInput({ addTask }: TodoInputProps) {
         placeholderTextColor="#B2B2B2"
         returnKeyType="send"
         selectionColor="#666666"
+        value={task}
         onChangeText={(text) => setTask(text)}
         onSubmitEditing={handleAddNewTask}
       />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,26 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedDoneTasks = tasks.map(task => ({ ...task, done: task.id === id ? !task.done : task.done }));
+
+    setTasks(updatedDoneTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const filteredTask = tasks.filter((task: Task) => task.id !== id)
+
+    setTasks(filteredTask)
   }
 
   return (
@@ -26,10 +37,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
